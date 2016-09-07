c1f0790dd1c221267fdc208fcb0d6c76e855a622
pedro.torres <pedro.torres@INNOWAVE-226>
2016-09-07 21:21:09 +0100

    building components and list page

--- client/app/components/pokemonlist/pokemonlist.js
+++ client/app/components/pokemonlist/pokemonlist.js
@@ -1,13 +1,15 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import pokemonlistComponent from './pokemonlist.component';
+import pokemonPreview from './pokemonpreview/pokemonpreview';
+import ngInfiniteScroll from 'ng-infinite-scroll';
 
 let pokemonlistModule = angular.module('pokemonlist', [
-    uiRouter
+    uiRouter,
+    ngInfiniteScroll,
+    pokemonPreview 
 ])
-
-
-
+    
     .config(($stateProvider, $urlRouterProvider) => {
         "ngInject";
 
