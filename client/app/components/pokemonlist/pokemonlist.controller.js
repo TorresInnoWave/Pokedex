80e58848dfb0b183edc196514ee3cace2f1dd2e4
pedro.torres <pedro.torres@INNOWAVE-226>
2016-09-07 20:58:57 +0100

    seting up routes

---
+++ client/app/components/pokemonlist/pokemonlist.controller.js
@@ -0,0 +1,26 @@
+class PokemonlistController {
+  constructor() {
+      this.name = 'pokemonlist';
+      this.pokemons = [];
+  }
+
+  getMore() {
+      for (var i = 0; i < 12; i++) {
+          this.pokemons.push({
+              "name": "bulbasaur",
+              "id": "001",
+              "types": [
+                  { "name": "grass" },
+                  { "name": "poison" }
+              ]
+          });
+      }
+  }
+
+  $onInit() {
+      this.getMore();
+  }
+
+}
+
+export default PokemonlistController;
