3735480b616e8b838eb61f3c23804c53335502ba
pedro.torres <pedro.torres@INNOWAVE-226>
2016-09-14 18:38:54 +0100

    git send

--- client/app/components/pokemonlist/pokemonlist.controller.js
+++ client/app/components/pokemonlist/pokemonlist.controller.js
@@ -1,18 +1,42 @@
 class PokemonlistController {
-  constructor() {
+    constructor(pokeapiClientService) {
+      'ngInject'; 
       this.name = 'pokemonlist';
       this.pokemons = [];
+      this.gettingPokemons = false;
+      this._pokeapiClientService = pokeapiClientService;
+      //console.log(pokeapiClientService.exampleFunction());
   }
 
   getMore() {
-      for (var i = 0; i < 12; i++) {
-          this.pokemons.push({
-              "name": "bulbasaur",
-              "id": "001",
-              "types": [
-                  { "name": "grass" },
-                  { "name": "poison" }
-              ]
+      //for (var i = 1; i < 720; i++){ 
+      //    var ids
+      //    if (i < 10)
+      //    {
+      //        ids = "00" + i;
+      //    }
+      //    else if (i <100)
+      //    {
+      //        ids = "0" + i;
+      //    }
+      //    else
+      //    {
+      //        ids = i;
+      //    }
+      //    this.pokemons.push({
+      //        "name": "bulbasaur",
+      //        "id": ids,
+      //        "types": [
+      //            { "name": "grass" },
+      //            { "name": "poison" }
+      //        ]
+      //    });
+      //}
+      if (!this.gettingPokemons) {
+          this.gettingPokemons = true;
+          this._pokeapiClientService.getPokemons(this.pokemons.length, (err, pokemons) => {
+              Array.prototype.push.apply(this.pokemons, pokemons);
+              this.gettingPokemons = false;
           });
       }
   }
